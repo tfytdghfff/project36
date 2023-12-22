edc997c4b3eb86175f75a4b34bc3bcce1ecabec5
tfytdghfff <134787200+tfytdghfff@users.noreply.github.com>
2023-12-22 18:32:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload()
+{}
+function setup()
+{
+    canvas = createCanvas(500,500);
+    canvas.center();
+
+}
+
+function draw()
+{}
+
+function take_snapshot()
+{
+    save("clown.png");
+}
